45628277125e72c1a82294e4952766a8111f7055
Gary Donovan <garyndonovan@hotmail.om>
2015-05-01 16:06:19 +1000

    added pycal.py

--- pycal.py
+++ pycal.py
@@ -20,19 +20,19 @@ def div(num1, num2):
 
 def main():
 	operation = input("What do you want to do? (/,*,+,-): ")
-	if (operation != '+' and operation != '-' and operation '*' and operation != '/'):
+	if (operation != '+' and operation != '-' and operation != '*' and operation != '/'):
 		#invalid operation
 		print ("you must enter a valid operation")
-	else
-	var1 = int(input("Enter num1"))
-	var2 = int(input("Enter num2"))
-	if(operation == "+"):
-		print (add(var1, var2))
-	elif(operation == "-"):
-		print (sub(var1, var2))
-	elif(operation == "/"):
-		print (div(var1, var2))
 	else:
-		print (mul(var1, var2))
+		var1 = int(input("Enter num1"))
+		var2 = int(input("Enter num2"))
+		if(operation == "+"):
+			print (add(var1, var2))
+		elif(operation == "-"):
+			print (sub(var1, var2))
+		elif(operation == "/"):
+			print (div(var1, var2))
+		else:
+			print (mul(var1, var2))
 
 main()
